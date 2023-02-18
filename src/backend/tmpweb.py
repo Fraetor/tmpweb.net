a15daadf657678548d5489484cf044e7112e3284
James Frost <git@frost.cx>
2023-02-18 12:24:49 +0000

    Add some SQL queries

--- src/backend/tmpweb.py
+++ src/backend/tmpweb.py
@@ -4,21 +4,33 @@ import secrets
 import shutil
 import sqlite3
 import time
-import tomllib
 import tempfile
 import os
+
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
 from . import safe_extractor
 
 db = sqlite3.connect("tmpweb.db")
 cur = db.cursor()
-# cur.execute("CREATE TABLE ")
+query = """CREATE TABLE IF NOT EXISTS sites(
+    site_id TEXT PRIMARY KEY,
+    creation_date INT,
+    expiry_date INT
+);
+"""
+cur.execute(query)
+db.commit()
 
 config = tomllib.load(Path("tmpweb_config.toml").read_bytes())
 
 
 def get_web_root(dir: Path) -> Path:
+    """Descend tree until we find either multiple directories or some files."""
     for root, dirs, files in os.walk(dir, followlinks=False):
-        # Descend tree until we find either multiple directories or some files.
         if len(dirs) >= 2 or len(files) > 0:
             return Path(root)
     raise ValueError("No files in archive.")
@@ -27,12 +39,12 @@ def get_web_root(dir: Path) -> Path:
 def create_site(archive_path: Path, retention_length: int = config.default_retention):
     # Generated URLs will have a 9 byte long base64 path. As base64 encodes 3 bytes
     # to 4 characters this gives us a nice length of URL whilst having sufficient
-    # keyspace for randomly finding a page to take many millennia.
+    # keyspace for randomly finding a site to take many millennia.
     site_id = secrets.token_urlsafe(9)
     creation_date = int(time.time())
     if retention_length > config.max_retention:
         retention_length = config.max_retention
-    expiry_time = creation_date + retention_length * 24 * 3600
+    expiry_date = creation_date + retention_length * 24 * 3600
     try:
         with tempfile.TemporaryDirectory as tmpdir:
             # Unzip/untar in a vaguely safe way.
@@ -60,22 +72,24 @@ def create_site(archive_path: Path, retention_length: int = config.default_reten
                     )
             web_root = get_web_root(tmpdir)
             # Record site in database
-            # https://docs.python.org/3/library/sqlite3.html
-            cur.execute("INSERT")
+            query = "INSERT INTO sites VALUES(?, ?, ?);"
+            cur.execute(query, site_id, creation_date, expiry_date)
+            db.commit()
             shutil.copytree(web_root, config.web_root, dirs_exist_ok=True)
-        # Return URL of site
         return {"status": 200, "content": f"https://{config.domain}/{site_id}/"}
-        # Return empty string if any prior step failed.
     except Exception as err:
         logging.error(err)
         return {"status": 500, "content": f"{err}"}
 
 
 def delete_old_sites():
-    # Sample through database and if the expiry_time is less than the current
-    # time delete the site's files, and remove its entry from the database.
-    # https://docs.python.org/3/library/sqlite3.html
-    cur = db.execute(
-        f"SELECT * FROM sites WHERE expiry_time IS GREATER THAN {int(time.time)}"
-    )
-    # Do something with the cursor.
+    query = "SELECT * FROM sites WHERE expiry_date < ?;"
+    for row in cur.execute(query, int(time.time)):
+        site_id = row[0]
+        shutil.rmtree(Path(config.web_root).joinpath(f"{site_id}/"))
+        cur.execute("DELETE FROM sites WHERE site_id IS ?", site_id)
+        db.commit()
+
+
+def handle_request():
+    pass
